93a8c198f5a8155b3b714814d5409e39d20d4432
Bharath-S-V <svbharath545@gmail.com>
2023-10-30 20:16:33 +0530

    product deletion

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,7 +11,8 @@ import {
   productDetailsReducer,
   productListReducer,
   productUpdateReducer,
-  productCreateReducer
+  productCreateReducer,
+  productDeleteReducer
 } from './reducers/productReducers';
 import {
   userRegisterReducer,
@@ -50,6 +51,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   productCreate: productCreateReducer,
 productUpdate: productUpdateReducer,
+productDelete: productDeleteReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
