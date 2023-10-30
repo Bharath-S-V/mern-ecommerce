e8152ebc8dabe290ab4086138d40f83c03f1c721
Bharath-S-V <svbharath545@gmail.com>
2023-10-30 18:06:36 +0530

    place order API

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import data from './data.js';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
+import orderRouter from './routers/orderRouter.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URL , {
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
