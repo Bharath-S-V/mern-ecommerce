0faf4b470488431043374c0da944e9a03701a23b
Bharath-S-V <svbharath545@gmail.com>
2023-10-30 19:38:21 +0530

    admin creation

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Redirect, Route } from 'react-router-dom';
+import { Route, Navigate } from 'react-router-dom';
 
 export default function PrivateRoute({ component: Component, ...rest }) {
   const userSignin = useSelector((state) => state.userSignin);
@@ -8,13 +8,7 @@ export default function PrivateRoute({ component: Component, ...rest }) {
   return (
     <Route
       {...rest}
-      render={(props) =>
-        userInfo ? (
-          <Component {...props}></Component>
-        ) : (
-          <Redirect to="/signin" />
-        )
-      }
-    ></Route>
+      element={userInfo ? <Component /> : <Navigate to="/signin" />}
+    />
   );
 }
